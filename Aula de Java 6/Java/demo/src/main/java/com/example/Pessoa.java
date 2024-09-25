fb06a81e51dd90f5f60b040e08533c198e4edfeb
Enzo Murajiro <104034917+murajiro@users.noreply.github.com>
2024-09-25 10:14:35 -0300

    Aula de java 6(Corrigir)

---
+++ Aula de Java 6/Java/demo/src/main/java/com/example/Pessoa.java
@@ -0,0 +1,27 @@
+public class Pessoa {
+    private String nome;
+    private float peso;
+    private String id;
+    private int idade;
+    
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public float getPeso(){
+        return peso;
+    }
+    public void setPeso(float peso){
+        this.peso = peso;
+    }
+    public int getIdade() {
+        return idade;
+    }
+    public void setIdade(int idade) {
+        this.idade = idade;
+
+    }
+    
+}    
